9c77c88d7e3dba1c9335d80484386d9c07852e57
anderson <andinho@gmail.com>
2022-05-20 14:38:04 -0300

    Initial commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import model.gato;
+
+import java.util.Arrays;
+
+public class primeiroprograma {
+    public static void main(String[] arqs) {
+        gato gato = new gato();
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 5;
+        System.out.println("hello world " + (a+b));*/
+
+    }
+
+}
